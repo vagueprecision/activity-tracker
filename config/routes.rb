d64e1a565f2f74ce884a454d82710982f0e643a3
SC028641 <schuyler.conover@cerner.com>
2019-01-05 09:41:02 -0500

    fixed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root :to => 'activities#index'
+
   resources :friends
   resources :goals
   resources :categories
   resources :auths
   resources :activities
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
